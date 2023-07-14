88ce08eb7317c4533f2ba561e85e3220ef7a2cd6
esperantkela <esperant.kela@gmail.com>
2023-07-14 17:05:09 +0100

    feat(button): disabled button if input is empty

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { styles } from "./components/global.style";
-import { Text, View, FlatList } from "react-native";
+import { Text, View, FlatList, Alert } from "react-native";
 import ProductList from "./components/ProductList";
 import AddProduct from "./components/AddProduct";
 
 export default function App() {
   const [Myproducts, setMyProducts] = useState([]);
+  const [btnDisabled, setBtnDisabled] = useState(true);
+
+  useEffect(() => {
+    if (product.length > 1) {
+      setBtnDisabled(false);
+    } else {
+      setBtnDisabled(true);
+    }
+  }, [product]);
 
   const deleteProduct = (key) => {
     setMyProducts((currentMyProducts) => {
@@ -22,7 +31,7 @@ export default function App() {
   };
   return (
     <View style={styles.container}>
-      <AddProduct submitHandler={submitHandler} />
+      <AddProduct submitHandler={submitHandler} disabled={btnDisabled} />
       <FlatList
         data={Myproducts}
         renderItem={({ item }) => (
