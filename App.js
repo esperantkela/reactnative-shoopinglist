79baa3dd2cf5e1b92578d550a0c8bbd31340168b
esperantkela <esperant.kela@gmail.com>
2023-07-17 10:36:17 +0100

    refactor(product): refactor product

--- App.js
+++ App.js
@@ -1,20 +1,11 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { styles } from "./components/global.style";
-import { Text, View, FlatList, Alert } from "react-native";
+import { View, FlatList } from "react-native";
 import ProductList from "./components/ProductList";
 import AddProduct from "./components/AddProduct";
 
 export default function App() {
   const [Myproducts, setMyProducts] = useState([]);
-  const [btnDisabled, setBtnDisabled] = useState(true);
-
-  useEffect(() => {
-    if (product.length > 1) {
-      setBtnDisabled(false);
-    } else {
-      setBtnDisabled(true);
-    }
-  }, [product]);
 
   const deleteProduct = (key) => {
     setMyProducts((currentMyProducts) => {
@@ -31,7 +22,7 @@ export default function App() {
   };
   return (
     <View style={styles.container}>
-      <AddProduct submitHandler={submitHandler} disabled={btnDisabled} />
+      <AddProduct submitHandler={submitHandler} />
       <FlatList
         data={Myproducts}
         renderItem={({ item }) => (
